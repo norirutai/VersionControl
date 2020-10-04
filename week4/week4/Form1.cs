e6fcb81640c5ff72216264a9d4c6214438d1ac0d
Nóra Rutai <nora.rutai@gmail.com>
2020-10-04 14:02:32 +0200

    6.

--- week4/week4/Form1.cs
+++ week4/week4/Form1.cs
@@ -7,22 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace week4
 {
-
+    
     public partial class Form1 : Form
+        
     {
         List<Flat> Flats;
         RealEstateEntities context = new RealEstateEntities();
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            
         }
         private void LoadData() 
         {
-            Flats = context.Flats.ToList(); 
+            Flats = context.Flats.ToList();
+        }
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+                //CreateTable();
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+
+                string errMsg = string.Format("Error: {0}\nLine:{1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Eror");
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+           
         }
     }
 }
