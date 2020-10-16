edd60f9434ff5563161925f2edd3b8704e983c71
Nóra Rutai <nora.rutai@gmail.com>
2020-10-16 10:12:18 +0200

    3

--- week6/week6/Form1.cs
+++ week6/week6/Form1.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week6.MnbServiceReference;
 
 namespace week6
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+            
+            
+
+        }
+        void GetExchangeRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var repose = mnbService.GetExchangeRates(request);
+            var result = repose.GetExchangeRatesResult;
         }
     }
+        
 }
