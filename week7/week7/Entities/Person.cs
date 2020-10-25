b0fb4515c508723d30c4f75189dea571f3e0b8c2
Nóra Rutai <nora.rutai@gmail.com>
2020-10-25 12:21:22 +0100

    4

--- week7/week7/Entities/Person.cs
+++ week7/week7/Entities/Person.cs
@@ -8,7 +8,7 @@ namespace week7.Entities
 {
     public class Person
     {
-        public int BirtYear { get; set; }
+        public int BirthYear { get; set; }
         public Gender Gender { get; set; }
         public int NbrOfChildren { get; set; }
         public bool IsAlive { get; set; }
